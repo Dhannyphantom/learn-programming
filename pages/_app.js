81bf5c043c7554de2a20bd32ee918323bcf23aea
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-08-04 22:54:41 +0100

    Empty Layout of the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config, library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+import "../styles/globals.css";
+
+config.autoAddCss = false;
+library.add(fas, far);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
