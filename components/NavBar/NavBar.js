22a8dc21aa8a411a5890b4faa3d1ff519291446e
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-08-04 23:15:55 +0100

    Basic NavBar done

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,5 +1,12 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav className={styles.nav}>
+      <Image src="/logo.png" className={styles.image} width={60} height="60" />
+      <h3>Learn Programming Enrollment</h3>
+      <h5>About</h5>
+    </nav>
+  );
 }
