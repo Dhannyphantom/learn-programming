d041d77d4c59fa5b94e558a41c2246b568b6f279
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-08-05 02:49:04 +0100

    Made the Card container responsive

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <nav className={styles.nav}>
       <Image src="/logo.png" className={styles.image} width={60} height="60" />
-      <h3>Learn Programming Enrollment</h3>
+      <h3>Learn Programming</h3>
       <h5>About</h5>
     </nav>
   );
