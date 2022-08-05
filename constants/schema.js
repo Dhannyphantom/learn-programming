cc7582dcbb9b3a90f4c37394b6aa3f6680d46052
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-08-05 15:56:34 +0100

    Created a basic yup validation schema for the form

---
+++ constants/schema.js
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+export const formInitials = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  gender: "",
+  hasPC: true,
+};
+
+export const formValidation = Yup.object().shape({
+  name: Yup.string().required().min(4).max(255).label("Full name"),
+  email: Yup.string().email().required().label("Email Address"),
+  phoneNumber: Yup.string().required().label("Phone number"),
+  gender: Yup.string().required().label("Gender"),
+  hasPC: Yup.bool().label("Own PC").required(),
+});
