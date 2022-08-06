d6ca87346a56a1ac74d5eab912aa9fbf5822cac9
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-08-06 22:21:49 +0100

    Properly added Google font and updated the form schema

--- constants/schema.js
+++ constants/schema.js
@@ -9,9 +9,9 @@ export const formInitials = {
 };
 
 export const formValidation = Yup.object().shape({
-  name: Yup.string().required().min(4).max(255).label("Full name"),
+  name: Yup.string().required().lowercase().min(4).max(255).label("Full name"),
   email: Yup.string().email().required().label("Email Address"),
   phoneNumber: Yup.string().required().label("Phone number"),
-  gender: Yup.string().required().label("Gender"),
-  hasPC: Yup.string().label("Own PC").required(),
+  gender: Yup.string().lowercase().required().label("Gender"),
+  hasPC: Yup.string().lowercase().label("Own PC").required(),
 });
