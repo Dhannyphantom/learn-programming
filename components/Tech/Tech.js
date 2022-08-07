5c740fa7b3bd58ec302d48b7e94e6403d7e08832
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-08-08 00:31:56 +0100

    Tech UI layout improved

--- components/Tech/Tech.js
+++ components/Tech/Tech.js
@@ -1,11 +1,29 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./Tech.module.css";
 import { TECH_DATA } from "../../constants/dataStore";
 
-export default function Tech() {
+const Item = ({ item }) => {
   return (
-    <div>
-      <h3>What You'll Learn</h3>
+    <div style={{ backgroundColor: item.bg }} className={styles.techItem}>
+      <Image src={item.image} width={70} height={70} />
+      {item.name && (
+        <p style={{ color: item.text }} className={styles.techName}>
+          {item.name}
+        </p>
+      )}
     </div>
+  );
+};
+
+export default function Tech() {
+  const renderTechs = TECH_DATA.map((obj) => {
+    return <Item key={obj.id} item={obj} />;
+  });
+  return (
+    <section className={styles.container}>
+      <h3>What You'll Learn</h3>
+      <div className={styles.tech}>{renderTechs}</div>
+    </section>
   );
 }
